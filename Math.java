8bc6d3e2012b3c8718040ce20422e6f3283688be
Jesbin-CMPG <se7en143canada@gmail.com>
2024-09-06 11:14:24 -0400

    updated

--- Math.java
+++ Math.java
@@ -8,5 +8,7 @@ public class Math {
         int y = 4;
 
         System.out.println("The Sum of 12 and 1 = " + (x + y));
+        System.out.println("The Diff of 12 and 1 = " + (x - y));
+        System.out.println("The Div of 12 and 1 = " + (x / y));
     }
 }
