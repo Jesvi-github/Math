e00f17b665bf939adbbc54f44f6fd7fe19d2ab6e
Jesbin-CMPG <se7en143canada@gmail.com>
2024-09-06 11:03:38 -0400

    math

---
+++ Math.java
@@ -0,0 +1,12 @@
+public class Math {
+
+    public static void main(String[] args) {
+
+        System.out.println("Welcome To the Calculator");
+
+        int x = 12;
+        int y = 4;
+
+        System.out.println("The Sum of 12 and 1 = " + (x + y));
+    }
+}
